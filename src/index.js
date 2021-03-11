06aae448d89bd3fdecf1017074e5189ee951587c
lu <919041098@qq.com>
2021-03-11 09:51:00 +0800

    'history'

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
-function router() {
-    let number = window.location.hash.substr(1);
-    let app = document.querySelector("#app");
-
-    number = number || 1;
-
-    //获取界面
-    let div = document.querySelector(`#div${number}`);
-    if(!div) {
-        div = document.querySelector("#div404");
-    }
-    div.style.display = "block";
-    //展示页面
-    if(app.children.length!==0) {
-        app.children[0].style.display = "none";
-        document.body.appendChild(app.children[0]);
-    }
-    app.appendChild(div);
+const app = document.querySelector("#app");
+const div1 = document.createElement('div')
+div1.innerHTML = '1'
+const div2 = document.createElement('div')
+div2.innerHTML = '2'
+const div3 = document.createElement('div')
+div3.innerHTML = '3'
+const div4 = document.createElement('div')
+div4.innerHTML = '4'
+const routeTable = {
+    '/1': div1,
+    '/2': div2,
+    '/3': div3,
+    '/4': div4
 }
 
-router();
-window.addEventListener("hashchange", () => {
+function router(container) {
+    let number = window.location.pathname
+    console.log(number);
+    if (number === '/') {
+        number = '/1'
+    }
+
+    //获取界面
+    let div = routeTable[number.toString()]
+    if (!div) {
+        div = document.querySelector("#div404");
+    }
+
+    //展示页面
+    container.innerHTML = ''
+    container.appendChild(div);
+}
+
+const Alla = document.querySelectorAll("a.link")
+for (let a of Alla) {
+    a.addEventListener("click", e => {
+        e.preventDefault()
+        const href = a.getAttribute("href")
+        window.history.pushState(null, `page ${href}`, href)
+        onStatechange(href)
+    })
+}
+
+router(app);
+
+function onStatechange() {
     console.log("hash 变了");
-    router();
-});
+    router(app);
+}
